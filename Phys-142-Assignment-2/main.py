ec810730e7edeeb1c8311c3c2695d0641f6c7226
Tristan Collis <tristan.collis@gmail.com>
2024-02-07 00:03:01 -0800

    Worked on problem d

--- Phys-142-Assignment-2/main.py
+++ Phys-142-Assignment-2/main.py
@@ -6,7 +6,7 @@ import problem_b
 import problem_c
 import problem_d
 
-problem_a.display(problem_a.run(3000), show=True)
+# problem_a.display(problem_a.run(3000), show=True)
 # problem_b.display(problem_b.run())
 
 # problem_d.display(problem_d.run(constants.ALPHA * np.arange(1, 10)), show=True)
--- Phys-142-Assignment-2/problem_d/problem_d.py
+++ Phys-142-Assignment-2/problem_d/problem_d.py
@@ -4,19 +4,19 @@ import numpy as np
 
 
 import constants
-from helpers import expectation, H_operator, x_operator
+from helpers import expectation, H_operator, x_operator, normalize
 
 
 def delta_E(
     psi_plus: np.ndarray[float, Any], psi_minus: np.ndarray[float, Any]
 ) -> float:
-    psi_symmetric = 2 ** -(1 / 2) * (psi_plus + psi_minus)
-    psi_asymmetric = 2 ** -(1 / 2) * (psi_plus - psi_minus)
+    psi_symmetric = 2**-0.5 * (psi_plus + psi_minus)
+    psi_asymmetric = 2**-0.5 * (psi_plus - psi_minus)
 
     E_0 = expectation(H_operator, psi_symmetric)
     E_1 = expectation(H_operator, psi_asymmetric)
 
-    return np.abs(E_1 - E_0)
+    return E_1 - E_0
 
 
 def t_tunnel(psi_initial: np.ndarray[complex, Any]) -> float:
@@ -24,23 +24,25 @@ def t_tunnel(psi_initial: np.ndarray[complex, Any]) -> float:
 
     psi_t = psi_initial
     expectation_x = np.zeros(timesteps)
-    
+
     for t in range(timesteps):
+        psi_t = normalize(constants.K @ psi_t)
         expectation_x[t] = expectation(x_operator, psi_t)
-        
-    return float(np.argmin(expectation_x) * constants.EPSILON)
+
+    return float(np.argmin(expectation_x))
 
 
 def run(
     alphas: np.ndarray[float, Any]
 ) -> tuple[np.ndarray[float, Any], np.ndarray[float, Any]]:
+
     delta_Es = np.zeros(alphas.shape)
     t_tunnels = np.zeros(alphas.shape)
 
-    x_mins = alphas ** -0.5
+    x_mins = alphas**-0.5
 
     for i, x_min in enumerate(x_mins):
-        
+
         psi_plus = (constants.OMEGA / np.pi) ** (1 / 4) * np.exp(
             -constants.OMEGA / 2 * (constants.X - x_min) ** 2
         )
@@ -51,12 +53,14 @@ def run(
         delta_Es[i] = delta_E(psi_plus, psi_minus)
         t_tunnels[i] = t_tunnel(psi_plus)
 
-
     return delta_Es, t_tunnels
 
 
-def display(data: tuple[np.ndarray[float, Any], np.ndarray[float, Any]], show: bool = False) -> None:
+def display(
+    data: tuple[np.ndarray[float, Any], np.ndarray[float, Any]], show: bool = False
+) -> None:
     delta_Es, t_tunnels = data
+
     plt.plot(delta_Es, t_tunnels)
 
     plt.title(r"$\Delta E vs t_{tunnel}$")
