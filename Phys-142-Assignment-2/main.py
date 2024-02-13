c989ad03c22aaba07b4be305d63e7d95de761edd
Tristan Collis <tristan.collis@gmail.com>
2024-02-12 20:46:06 -0800

    Restructured problem a to use vectorized computations. Applied corrections.

--- Phys-142-Assignment-2/main.py
+++ Phys-142-Assignment-2/main.py
@@ -1,20 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import constants
+import constants as const
 import helpers
 import problem_a
 import problem_b
 import problem_c
 import problem_d
 
-# problem_a.display(problem_a.run(1500), show=True)
-# problem_b.display(problem_b.run())
-alphas = constants.ALPHA * np.linspace(0.5, 5, 10).reshape(-1,1,1)
-Es_v, Ts_v = problem_d.run_vectorized(alphas=alphas, timesteps=5)
-plt.plot(alphas.flat, Es_v.flat)
-plt.show()
-plt.clf()
+# problem_a.display(*problem_a.run(), show=True, save=True)
 
-plt.plot(alphas.flat, Ts_v.flat)
-plt.show()
-plt.clf()
+# problem_b.display(problem_b.run())
+
+problem_c.run()
+
+# alpha = const.ALPHA * np.linspace(1, 2, 9).reshape(-1,1,1)
+# problem_d.display(problem_d.run(alpha=alpha), show=True, save=True)
--- Phys-142-Assignment-2/problem_c/problem_c.py
+++ Phys-142-Assignment-2/problem_c/problem_c.py
@@ -1,6 +1,18 @@
-def run() -> None:
-    ...
+import numpy as np
+
+import constants as const
+from helpers import animate, K_0, normalize
 
 
-def display() -> None:
-    ...
+def run(filename: str = "problem_c.mp4") -> None:
+    psi_t = const.PSI_INITIAL
+    psi_pdf = np.zeros((const.NT + 1, const.ND + 1))
+
+    for t in range(const.NT + 1):
+        psi_pdf[t] = (np.abs(psi_t) ** 2).flat
+        psi_t = normalize(K_0 @ psi_t)
+
+    animate(np.squeeze(psi_pdf), np.squeeze(const.X), filename)
+
+
+def display() -> None: ...
